16ab76d93fb4f9c3ae868087c7864e62ec670ca8
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-06 22:30:42 +0300

    Initial commit: Python RAWSim-O MVP with 2D/3D visualization

---
+++ visualization/__init__.py
@@ -0,0 +1,10 @@
+"""
+RAWSim-O MVP Visualization Module
+
+Visualization components for 2D and 3D rendering.
+"""
+
+from .renderer_2d import Renderer2D
+from .renderer_3d import Renderer3D
+
+__all__ = ['Renderer2D', 'Renderer3D']
